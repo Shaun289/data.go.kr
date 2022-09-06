0d9b19cfb3e4638300a21ddfa5c115cebaa01f78
sjpark <sjpark@idis.co.kr>
2022-09-06 18:23:01 +0900

    Feature: SeoulGanseo draw plot

--- cctv_event/SeoulGangseo/seoul_gangseo_20200930.py
+++ cctv_event/SeoulGangseo/seoul_gangseo_20200930.py
@@ -2,12 +2,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # https://stackoverflow.com/questions/42372617/how-to-plot-csv-data-using-matplotlib-and-pandas-in-python
-pd_data = pd.read_csv("./seoul_gangseo_data_20200930.utf-8.csv")
-print(pd_data)
+df = pd.read_csv("./seoul_gangseo_data_20200930.utf-8.csv", parse_dates=[['이벤트발생연도','이벤트발생월','이벤트발생일','이벤트발생시간']])
+df.rename(columns={"이벤트발생연도_이벤트발생월_이벤트발생일_이벤트발생시간":"이벤트발생시간"}, inplace=True); 
 
-pd_data.plot(
+# 이벤트발생시간     이벤트분류  지구코드  발생건수
 
-#plt.plot([1,2,3,4]);
-#plt.show()
+#df[df["이벤트분류"]=="FCLFT101"].plot(x="이벤트발생시간", y="발생건수")
+df.plot(x="이벤트발생시간", y="발생건수")
+plt.show()
+
 
 #plt.savefig('cctv_event_metadata_20200930.png');
